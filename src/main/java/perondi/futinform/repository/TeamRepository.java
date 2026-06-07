ad45d794d15981fbf30e5cac00bb939aee739196
perondi09 <guilhermeperondi09@gmail.com>
2026-06-07 20:56:55 -0300

    feat: Create new repository for register system

--- src/main/java/perondi/futinform/repository/TeamRepository.java
+++ src/main/java/perondi/futinform/repository/TeamRepository.java
@@ -3,8 +3,16 @@ package perondi.futinform.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import perondi.futinform.entity.Team;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
+
     Optional<Team> findByExternalId(Long externalId);
+
+    // Busca todos os times de uma liga pelo código
+    List<Team> findByLeagueCodeOrderByNameAsc(String leagueCode);
+
+    // Busca times pelo nome (para o usuário pesquisar)
+    List<Team> findByNameContainingIgnoreCaseOrderByNameAsc(String name);
 }
---
+++ src/main/java/perondi/futinform/repository/UserRepository.java
@@ -0,0 +1,16 @@
+package perondi.futinform.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import perondi.futinform.entity.User;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
+}
