b269cac1383bf12d82203482bc5e86bca6c24138
Guilherme <guilhermeperondi09@gmail.com>
2026-07-19 14:49:04 -0300

    feat: create global excpetion

---
+++ src/main/java/perondi/futinform/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+package perondi.futinform.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        Map<String, String> errorResponse = new HashMap<>();
+        errorResponse.put("erro", ex.getMessage());
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+}
